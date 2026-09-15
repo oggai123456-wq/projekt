fe037e6756bd774ac11855c45e98198286c4ea26
oggai123456-wq <oggai123456@gmail.com>
2026-09-15 15:12:52 +0200

    2

---
+++ logic.js
@@ -0,0 +1,20 @@
+let cookieButton = document.querySelector("#CookieButton")
+let score = document.querySelector("#Score")
+let cashMulti = document.querySelector("#CashMulti")
+let upgrade1 = document.querySelector("#Upgrade1")
+
+let cashMultiValue = 1.5
+
+
+
+
+
+let scoreValue = 0
+
+cookieButton.addEventListener("click", function() {
+    score.innerText = scoreValue ++ * cashMultiValue
+})
+
+upgrade1.addEventListener("click", function() {
+
+})
