1010956551863b61b6d9c02e25279605ede9417f
oggai123456-wq <oggai123456@gmail.com>
2026-09-17 11:21:12 +0200

    update1

--- logic.js
+++ logic.js
@@ -17,7 +17,7 @@ let currentUpgrade3 = 1
 
 let upgrade1Price = 10
 let upgrade2Price = 25
-let upgrade3Prie = 120
+let upgrade3Price = 120
 // ------------------------------
 let scoreValue = 0
 let cash = scoreValue
@@ -28,7 +28,7 @@ let Buystatus = document.querySelector("#status")
 
 cookieButton.addEventListener("click", function() {
     cash += cashPerClick * cashMultiValue
-    score.innerText = Math.round(cash)
+    score.innerText = Math.round(cash).toLocaleString()
 
     // Start animation
     cookieButton.classList.remove("clicked")
@@ -45,7 +45,7 @@ upgrade1.addEventListener("click", function() {
 
     if(cash >= price){
     cash -= price
-    score.innerText = Math.round(cash)
+    score.innerText = Math.round(cash).toLocaleString()
 
     cashMultiValue = Math.round((cashMultiValue + 0.5) * 10) / 10
     cashMulti.textContent = "Cash Multiplier: " + cashMultiValue
@@ -81,6 +81,32 @@ upgrade2.addEventListener("click", function() {
     pricecount = upgrade2Price * currentUpgrade2
     price = pricecount * currentUpgrade2
     upgrade2.innerText = "Cheaper Groceries: " + price
+    Buystatus.innerText = "good"
+    }else{
+        Buystatus.innerText = "Error"
+    }
+
+})
+
+upgrade3.addEventListener("click", function() {
+    let pricecount = upgrade3Price * currentUpgrade3
+    let price = pricecount * currentUpgrade3
+
+    console.log(cash)
+    console.log(price)
+
+    if(cash >= price){
+    cash -= price
+    score.innerText = Math.round(cash)
+
+    cashMultiValue = Math.round((cashMultiValue + 1.25) * 10) / 10
+    cashMulti.textContent = "Cash Multiplier: " + cashMultiValue
+
+    currentUpgrade3 += 1
+
+    pricecount = upgrade3Price * currentUpgrade3
+    price = pricecount * currentUpgrade3
+    upgrade3.innerText = "Cheaper Groceries: " + price
     Buystatus.innerText = "good"
     }else{
         Buystatus.innerText = "Error"
